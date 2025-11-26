b6a76a3d4b605707d8a1769dd641e47530dbcc4f
Maximiliano Robledo <maxrobledo13@tamu.edu>
2025-11-25 22:24:17 -0600

    add valid cli arguments and task statuses

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// CLI arguments
+const (
+	add            string = "add"
+	update         string = "update"
+	delete         string = "delete"
+	list           string = "list"
+	markInProgress string = "mark-in-progress"
+	markDone       string = "mark-done"
+)
+
+// task statuses
+const (
+	taskDone       string = "done"
+	taskInProgress string = "todo"
+	taskNotDone    string = "in-progress"
+)
 
 func main() {
 	fmt.Println("Hello World")
